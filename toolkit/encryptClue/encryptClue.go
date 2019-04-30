170210f0cfc7e2710565a57acd48f1ca2d2aadd0
alicespyglass <alicespyglass@gmail.com>
2019-04-30 08:46:20 +0100

    Update encode to encryp clue with createHash and encrypt functions. Rename file. Delete binary

---
+++ toolkit/encryptClue/encryptClue.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+)
+
+func createHash(key string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func encrypt(data []byte, passphrase string) []byte {
+	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error)
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error)
+	}
+	cipherText := gcm.Seal(nonce, nonce, data, nil)
+	return cipherText
+}
+
+func main() {
+	data := []byte(os.Args[1])
+	passphrase := os.Args[2]
+	cipher := encrypt(data, passphrase)
+	fmt.Printf("cipher: %v", cipher)
+}
