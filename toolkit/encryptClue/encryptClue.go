e0b40761957a50625f4602f332a70797b88e5d56
alicespyglass <alicespyglass@gmail.com>
2019-05-05 17:22:26 +0100

    add annotations

--- toolkit/encryptClue/encryptClue.go
+++ toolkit/encryptClue/encryptClue.go
@@ -19,31 +19,50 @@ func createHash(key string) []byte {
 }
 
 func encrypt(data []byte, passphrase string) []byte {
-	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
+	// create an AES block cipher with hashed passphrase
+	key := []byte(createHash(passphrase))
+	block, _ := aes.NewCipher(key)
+
+	// wrap block in Galois Counter Mode
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		panic(err.Error())
 	}
+
+	// create a nonce of length specified by GCM
 	nonce := make([]byte, gcm.NonceSize())
 	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
 		panic(err.Error())
 	}
+
+	// Seal creates authenticated cipherText using a nonce and cipher block (data)
+	// nonce used for decryption must be the same as one used in encryption
+	// first paramter in Seal is a prefix value which encrypted data is appended to. We prepend nonce here
+	// last paramter is for any additional data such as headers
 	ciphertext := gcm.Seal(nonce, nonce, data, nil)
 	return ciphertext
 }
 
 func decrypt(data []byte, passphrase string) []byte {
+	// create an AES block cipher with hashed passphrase
 	key := []byte(createHash(passphrase))
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		panic(err.Error())
 	}
+
+	// wrap block in Galois Counter Mode
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		panic(err.Error())
 	}
+
+	// create a nonce of length specified by GCM.
+	// remember we'd prefixed the nonce with the data. Now separate from ciphertext
 	nonceSize := gcm.NonceSize()
 	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+
+	// decrypt data with Open and return as plaintext
 	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
 		panic(err.Error())
