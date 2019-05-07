1ec5e0f7d5e6197d39e27fb5295766071fbdadc8
alicespyglass <alicespyglass@gmail.com>
2019-05-07 20:02:57 +0100

    decryptClue - add annotations

--- toolkit/decryptClue/decryptClue.go
+++ toolkit/decryptClue/decryptClue.go
@@ -4,33 +4,41 @@ import (
 	"GODETECTIVE/toolkit/createHash"
 	"crypto/aes"
 	"crypto/cipher"
+	"fmt"
 	"io/ioutil"
 )
 
 func Decrypt(data []byte, passphrase string) []byte {
-	// create an AES block cipher with hashed passphrase
+	// 1. Create a cryptographic hash with the passphrase
 	key := []byte(createHash.Hash256(passphrase))
+	fmt.Printf("1. hash key: %x\n", key)
+
+	// 2. Create an AES block cipher with the hash
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		panic(err.Error())
 	}
+	fmt.Printf("2. aes cipher block: %x\n", block)
 
-	// wrap block in Galois Counter Mode
+	// 3. Wrap block in Galois Counter Mode
 	gcm, err := cipher.NewGCM(block)
+	fmt.Printf("3. gcm: %v\n", gcm)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	// create a nonce of length specified by GCM.
-	// remember we'd prefixed the nonce with the data. Now separate from ciphertext
+	// 4. Remember we'd prefixed the nonce with the data. Now separate from ciphertext
 	nonceSize := gcm.NonceSize()
 	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	fmt.Printf("4a. nonce: %v\n", nonce)
+	fmt.Printf("4b. ciphertext: %x\n", nonce)
 
-	// decrypt data with Open and return as plaintext
+	// 5. Decrypt data to plaintext with Open and the nonce and ciphertext
 	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
 		panic(err.Error())
 	}
+	fmt.Printf("5. plaintext: %x\n", plaintext)
 	return plaintext
 }
 
