d427e7e3e338cdc46a9c1b5edb669703826dd234
alicespyglass <alicespyglass@gmail.com>
2019-05-07 21:20:15 +0100

    createHash - tidy - delete print

--- toolkit/createHash/createHash.go
+++ toolkit/createHash/createHash.go
@@ -2,12 +2,10 @@ package createHash
 
 import (
 	"crypto/sha256"
-	"fmt"
 )
 
 func Hash256(key string) []byte {
 	hasher := sha256.New()
 	hasher.Write([]byte(key))
-	fmt.Printf("v hash.Sum base16: %x\n", hasher.Sum(nil))
 	return hasher.Sum(nil)
 }
